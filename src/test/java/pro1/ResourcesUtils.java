c1d23421dfb8e07c6eaeeb2e49b29a173d6020f8
Tomáš Šafařík <safarto1@uhk.cz>
2026-04-30 18:04:28 +0200

    Další úpravy

--- src/test/java/pro1/ResourcesUtils.java
+++ src/test/java/pro1/ResourcesUtils.java
@@ -8,10 +8,6 @@ import java.nio.file.Paths;
 public class ResourcesUtils {
     public static String readResourceFile(String path){
         try {
-            var directPath = Paths.get("src", "test", path);
-            if (Files.exists(directPath)) {
-                return Files.readString(directPath);
-            }
             return Files.readString(Paths.get("src","test", "resources", path));
         } catch (IOException e) {
             throw new RuntimeException(e);
